6d2d9f3519dfcaf7ce2e05385b21edac116fa9ba
vzharov <zharov@omnicomm.ru>
2025-02-25 19:09:06 +0300

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,21 +16,25 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <Layout>
-    <div className="min-h-screen flex flex-col bg-gray-100 p-4">
+      <div className="min-h-screen flex flex-col bg-gray-100 p-4">
 
-      {/* Основной контент */}
-      <div className="flex flex-col items-center justify-center flex-grow">
-        <h1 className="text-4xl font-bold text-gray-800">Портфолио иллюстратора</h1>
-        <p className="text-lg text-gray-600 mt-2">Добро пожаловать! Здесь собраны лучшие работы.</p>
+        {/* Основной контент */}
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <h1 className="text-4xl font-bold text-gray-800">Illustrator Liza Gomzyakova</h1>
+          <div className="text-lg text-gray-600 mt-2">
+            <p>🌱Преподаю ребятишкам</p>
+            <p>🌱Выпускница Школы Детской Книги</p>
+            <p>🌱Иллюстрирую книги для детей</p>
+          </div>
 
-        {/* Галерея */}
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-6">
-          <img src="/illustrations/work1.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105"/>
-          <img src="/illustrations/work2.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105"/>
-          <img src="/illustrations/work3.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105"/>
+          {/* Галерея */}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-6">
+            <img src="/illustrations/work1.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105" />
+            <img src="/illustrations/work2.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105" />
+            <img src="/illustrations/work3.jpg" className="rounded-lg shadow-md transform transition duration-300 hover:scale-105" />
+          </div>
         </div>
       </div>
-    </div>
     </Layout>
   );
 }
